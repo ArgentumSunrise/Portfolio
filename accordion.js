347792f07f353e69b12aa8a192e5c72597eccf6c
Colter Giem <coltergiem@gmail.com>
2015-07-02 14:19:12 -0600

    Just changed everything
    BECAUSE YOLO, BITCH

--- accordion.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('div.acc').click(function (event) {
-        var target = $(event.target).parent();
-        target.children(".acc-content").slideToggle(700);
-    });
-});
---
+++ main.js
@@ -0,0 +1,15 @@
+/* $(document).ready(function () {
+    $(window).scroll(function () {
+        $('.fadeIn').each(function (i) {
+            var elemBottom = $(this).offset().top + $(this).outerHeight();
+            var windowBottom = $(window).scrollTop() + $(window).height();
+            if (windowBottom > elemBottom) {
+                $(this).animate({
+                    'opacity': '1'
+                }, 300);
+            }
+        });
+    });
+}); */
+
+// I'll figure this out later
