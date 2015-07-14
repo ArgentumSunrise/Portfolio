e072e48fc921430b36ae750dd6e81729d372d981
Colter Giem <coltergiem@gmail.com>
2015-07-13 22:19:11 -0600

    Changed some style aspects

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-/* $(document).ready(function () {
+/*$(document).ready(function () {
     $(window).scroll(function () {
         $('.fadeIn').each(function (i) {
             var elemBottom = $(this).offset().top + $(this).outerHeight();
             var windowBottom = $(window).scrollTop() + $(window).height();
             if (windowBottom > elemBottom) {
-                $(this).animate({
-                    'opacity': '1'
-                }, 300);
+                $(this).fadeTo(1, 300);
             }
         });
     });
-}); */
-
-// I'll figure this out later
+});*/
