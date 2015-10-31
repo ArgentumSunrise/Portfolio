146e4b46bc5c4ea39814c3e1a6065cb91bed5cf7
ArgentumSunset <coltergiem@gmail.com>
2015-10-31 13:36:13 -0600

    Add parallax, links

--- js/main.js
+++ js/main.js
@@ -4,15 +4,27 @@ $(document).ready(function () {
             "transform", "rotateX(90deg)"
         );
         $(this).children('.nav-selected-ctr').css(
-            "transform", "rotateX(0)"
+            "transform", "rotateX(0deg)"
         );
     });
     $('.nav-item').mouseleave(function () {
         $(this).children('.nav-unselected-ctr').css(
-            "transform", "rotateX(0)"
+            "transform", "rotateX(0deg)"
         );
         $(this).children('.nav-selected-ctr').css(
             "transform", "rotateX(90deg)"
         );
+    });
+
+    $('.nav-p').click(function (event) {
+        var target = "#" + $(this).parent().attr('id') + "-page";
+        if (target.length) {
+            event.preventDefault();
+            console.log(target)
+            $(window).animate({
+                scrollTop: 600
+            }, 1500);
+            console.log($(target).offset().top)
+        }
     });
 })
