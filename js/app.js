406e007415bd03f6f97fb5704eb49ff292dd7983
ArgentumSunset <coltergiem@gmail.com>
2016-03-27 10:00:13 -0600

    Changed homepage design

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
-var atHome = true;
-
 $(document).ready(function () {
     var page = location.search.substring(1, 2000);
     if (page) {
         $('#content').load('/pages/' + page + '.html');
     } else {
-        $('#content').load('/pages/home.html', function () {
-            cloudRush();
-        });
+        $('#content').load('/pages/home.html');
+
     }
 
-    $('#nav').find('.navitem').click(function () {
+    $('.navitem').click(function () {
         id = this.id;
         console.log(this.id);
-        $('#content').load('/pages/' + id + '.html').hide().fadeIn();
+        $('#content').load('/pages/' + id + '.html', function () {
+            $('#small-menu').slideUp();
+            if (id != "home") {
+                $('#hamburger').toggleClass('open').css('background-color', '#222');
+                $('a').css('color', '#222');
+            } else {
+                $('a').css('color', '#fff');
+                $('#hamburger').css('background-color', '#fff');
+            }
+        }).hide().fadeIn();
         window.history.pushState("string", "Title", "/?" + id);
     });
 });
