131ab94066f2539369d7b67578d46727f78a83c4
ArgentumSunset <coltergiem@gmail.com>
2016-03-28 21:41:47 -0600

    Various design edits, made home sidebar color-responsive, started process page

---
+++ js/animations.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    var fpaths = document.querySelectorAll('*[id^="fpath"]'),
+        spaths = document.querySelectorAll('*[id^="path"]'),
+        logo = document.querySelectorAll('*[id^="logopath"]'),
+        lengths = new Array(),
+        pages = ["front", "info", "skills", "process", "resume"];
+
+    $('#lines').hide().fadeIn(500);
+
+    prepPaths(fpaths);
+    prepPaths(spaths);
+    prepPaths(logo);
+    setTimeout(function () {
+        draw(fpaths);
+        setTimeout(function () {
+            draw(spaths)
+        }, 500);
+        setTimeout(function () {
+            draw(logo)
+        }, 2500);
+    }, 500);
+
+    function prepPaths(paths) {
+        lengths = [];
+        for (var i = 0; i < paths.length; i++) {
+            l = paths[i].getTotalLength();
+            lengths[i] = l;
+            paths[i].style.strokeDasharray = paths[i].getTotalLength() + ' ' + paths[i].getTotalLength();
+            paths[i].style.strokeDashoffset = paths[i].getTotalLength();
+            paths[i].style.transition = paths[i].style.WebkitTransition =
+                'none';
+        }
+    }
+
+    function draw(paths) {
+        for (var j = 0; j < paths.length; j++) {
+            paths[j].style.transition = paths[j].style.WebkitTransition =
+                'stroke-dashoffset 3s ease-in-out';
+            paths[j].style.strokeDashoffset = '0';
+        }
+    }
+
+    $('.sidebar .node').mouseenter(function () {
+        $(this).parent().children('a').fadeTo(100, 1);
+    }).mouseleave(function () {
+        $(this).parent().children('a').fadeTo(100, 0);
+    });
+
+    $('.node').click(function (event) {
+        var id = $(this).attr('id');
+        var target = $("#" + id + "-sec");
+        if (target.length) {
+            event.preventDefault();
+            top.location.href = '#' + target.attr('id');
+        }
+    });
+
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $(function () {
+            setTimeout(function () {
+                $('#lines h1').fadeTo('slow', 1);
+            }, 5500);
+        });
+    }
+});
--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $('#hamburger').show();
+        $('#nav-right').hide();
+    }
+    $(document).ready(function () {
+        $('#hamburger').click(function () {
+            $(this).toggleClass('open');
+            $('#small-menu').slideToggle(300);
+        });
+    });
+
     var page = location.search.substring(1, 2000);
     if (page) {
-        $('#content').load('/pages/' + page + '.html');
+        $('#content').load('../pages/' + page + '.html');
     } else {
-        $('#content').load('/pages/home.html');
-
+        $('#content').load('../pages/home.html');
     }
 
     $('.navitem').click(function () {
@@ -13,11 +23,11 @@ $(document).ready(function () {
         $('#content').load('/pages/' + id + '.html', function () {
             $('#small-menu').slideUp();
             if (id != "home") {
-                $('#hamburger').toggleClass('open').css('background-color', '#222');
-                $('a').css('color', '#222');
+                $('#hamburger').toggleClass('open').children('span').css('background-color', '#222');
+                $('#nav a').css('color', '#222');
             } else {
-                $('a').css('color', '#fff');
-                $('#hamburger').css('background-color', '#fff');
+                $('#nav a').css('color', '#fff');
+                $('#hamburger span').css('background-color', '#fff');
             }
         }).hide().fadeIn();
         window.history.pushState("string", "Title", "/?" + id);
